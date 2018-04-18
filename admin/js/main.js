24cda211ddf924992456daccd5002001a381a9a2
caloloc2 <calolomino@gmail.com>
2018-04-17 19:52:22 -0500

    actualiza

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,6 +1,6 @@
 function Leer(){
 	$.ajax({
-		url: 'http://controlraspi.ddns.net/control_raspi/php/obtener_datos.php',
+		url: 'http://controlraspi.ddns.net/php/obtener_datos.php',
 		dataType: 'json',		
 		async: false,
 		success: function(datos) {
