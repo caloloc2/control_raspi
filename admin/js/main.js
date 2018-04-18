30f08c47db849af0b77ff2ca5a4f05503e94231e
caloloc2 <calolomino@gmail.com>
2018-04-17 19:34:38 -0500

    actualiza

---
+++ admin/js/main.js
@@ -0,0 +1,13 @@
+function Leer(){
+	$.ajax({
+		url: '192.168.0.105/control_raspi/php/obtener_datos.php',
+		dataType: 'json',		
+		async: false,
+		success: function(datos) {
+			console.log(datos);
+		},
+		error:function(e){
+			console.log(e.responseText);
+		}
+	});	
+}
