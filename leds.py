f8de2a46346c83d7b9b5822a795dd6dbd3686d16
caloloc2 <calolomino@gmail.com>
2018-08-13 11:48:52 -0500

    actualiza

--- leds.py
+++ leds.py
@@ -1,13 +1,25 @@
 import RPi.GPIO as GPIO
 import time
+import MySQLdb
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.OUT) ## GPIO 17 como salida
 GPIO.setup(19, GPIO.OUT) ## GPIO 27 como salida
 GPIO.setup(13, GPIO.OUT) ## GPIO 27 como salida
 GPIO.setup(6, GPIO.OUT) ## GPIO 27 como salida
 
+def Lectura():
+	db = MySQLdb.connect(host="34.223.215.43", user="root", passwd="385402292Mica_02", db="control_raspi")
+	cur = db.cursor()
+	cur.execute("SELECT * FROM control LIMIT 1")
+	for row in cur.fetchall() :
+		print row
+	cur.close()
+	db.close ()
+
 try:
 	while True:
+		Lectura()
 		GPIO.output(26, True)
 		time.sleep(1)
 		GPIO.output(19, True)
