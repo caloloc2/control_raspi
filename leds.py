36f35a40a3a1f6c2854121f450162d71fd2868ae
caloloc2 <calolomino@gmail.com>
2018-08-13 11:36:15 -0500

    actualiza

--- leds.py
+++ leds.py
@@ -1,7 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT) ## GPIO 17 como salida
-GPIO.setup(27, GPIO.OUT) ## GPIO 27 como salida
+GPIO.setup(26, GPIO.OUT) ## GPIO 17 como salida
+GPIO.setup(19, GPIO.OUT) ## GPIO 27 como salida
+GPIO.setup(13, GPIO.OUT) ## GPIO 27 como salida
+GPIO.setup(6, GPIO.OUT) ## GPIO 27 como salida
 
-print('valio')
+while(True)
+	GPIO.output(26, True)
+	time.sleep(1)
+	GPIO.output(19, True)
+	time.sleep(1)
+	GPIO.output(13, True)
+	time.sleep(1)
+	GPIO.output(6, True)
+	time.sleep(1)
+	GPIO.output(26, False)
+	GPIO.output(26, False)
+	GPIO.output(26, False)
+	GPIO.output(26, False)
+	time.sleep(1)
