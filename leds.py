c032b85f88cf3a223f2016155c4c9f031542a493
caloloc2 <calolomino@gmail.com>
2018-08-13 13:42:27 -0500

    actualiza

--- leds.py
+++ leds.py
@@ -8,46 +8,41 @@ GPIO.setup(19, GPIO.OUT) ## GPIO 27 como salida
 GPIO.setup(13, GPIO.OUT) ## GPIO 27 como salida
 GPIO.setup(6, GPIO.OUT) ## GPIO 27 como salida
 
-def xxxx():
+def lectura():
 	db = MySQLdb.connect(host="34.223.215.43", user="root", passwd="385402292Mica_02", db="control_raspi")
 	cur = db.cursor()
 	cur.execute("SELECT * FROM control LIMIT 1")
 	for row in cur.fetchall() :
+		# obtiene el estado de los pines
+		estados = str(row[8])
+		valor = estados.split('-')
+
 		# led 1
-		if (row[1]==1):
+		if (valor[0]==1):
 			GPIO.output(26, True)
 		else:
 			GPIO.output(26, False)
 		# led 1
-		if (row[2]==1):
+		if (valor[1]==1):
 			GPIO.output(19, True)
 		else:
 			GPIO.output(19, False)
 		# led 1
-		if (row[3]==1):
+		if (valor[2]==1):
 			GPIO.output(13, True)
 		else:
 			GPIO.output(13, False)
 		# led 1
-		if (row[4]==1):
+		if (valor[3]==1):
 			GPIO.output(6, True)
 		else:
 			GPIO.output(6, False)
 	cur.close()
 	db.close ()
 
-def leds():
-	dato = ''
-	hora = ''
-	archivo = open("http://controlraspi.ddns.net/php/leds.txt", "r") 
-	for linea in archivo.readlines():		
-		dato+= linea
-
-	print dato
-
 try:
 	while True:
-		leds()
+		lectura()
 		time.sleep(1)
 except KeyboardInterrupt:
 	print "Script finalizado."
