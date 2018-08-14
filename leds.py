b6a1bf1c41643bf417f64752e2f58b94637ae6c6
caloloc2 <calolomino@gmail.com>
2018-08-13 20:11:33 -0500

    actualiza

--- leds.py
+++ leds.py
@@ -13,42 +13,65 @@ def lectura():
 	cur = db.cursor()
 	cur.execute("SELECT * FROM control LIMIT 1")
 	for row in cur.fetchall() :
-		estados = (str(row[8])).split('*')
+		estados = (str(row[8])).split('*')		
 		audio_activo = row[7]
 
 		if (audio_activo==1):
 			for linea in estados:
 				# obtiene el estado de los pines
-				valor = linea.split('-')
+				row = linea.split('-')
 
 				# led 1
 				if (valor[0]=='1'):
 					GPIO.output(26, True)
 				else:
 					GPIO.output(26, False)
-				# led 1
+				# led 2
 				if (valor[1]=='1'):
 					GPIO.output(19, True)
 				else:
 					GPIO.output(19, False)
-				# led 1
+				# led 3
 				if (valor[2]=='1'):
 					GPIO.output(13, True)
 				else:
 					GPIO.output(13, False)
-				# led 1
+				# led 4
 				if (valor[3]=='1'):
 					GPIO.output(6, True)
 				else:
 					GPIO.output(6, False)
 
-				time.sleep(0.5)
+				time.sleep(0.2)
+		else:
+			# led 1
+			if (row[1]==1):
+				GPIO.output(26, True)
+			else:
+				GPIO.output(26, False)
+			# led 2
+			if (row[2]==1):
+				GPIO.output(19, True)
+			else:
+				GPIO.output(19, False)
+			# led 3
+			if (row[3]==1):
+				GPIO.output(13, True)
+			else:
+				GPIO.output(13, False)
+			# led 4
+			if (row[4]==1):
+				GPIO.output(6, True)
+			else:
+				GPIO.output(6, False)
+			time.sleep(0.5)
 	cur.close()
 	db.close ()
 
 try:
 	while True:
 		lectura()
+		time.sleep(0.2)
 except KeyboardInterrupt:
 	print "Script finalizado."
 	GPIO.cleanup();
