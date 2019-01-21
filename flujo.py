b5bee5ba8d2895a6432e4b96be7641c015bbbcf3
caloloc2 <calolomino@gmail.com>
2019-01-21 16:51:07 -0500

    actualiza

--- flujo.py
+++ flujo.py
@@ -13,7 +13,7 @@ def flujo():
     db = MySQLdb.connect(host="34.223.215.43", user="root", passwd="385402292Mica_02", db="control_raspi")
     cur = db.cursor()
     cur.execute("UPDATE control SET nivel_agua='"+str(estado_flujo)+"'")
-    cur.commit()
+    db.commit()
     #print "UPDATE control SET nivel_agua='"+str(estado_flujo)+"'"
 
 try:
