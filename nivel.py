f2b63afaa768a167230feda7f2993efd6f525a7d
caloloc2 <calolomino@gmail.com>
2019-01-21 17:06:10 -0500

    actualiza

--- nivel.py
+++ nivel.py
@@ -3,18 +3,19 @@ import time
 import MySQLdb
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP) ## GPIO 22 como salida
+#GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP) ## GPIO 22 como entrada
+GPIO.setup(22, GPIO.IN) ## GPIO 22 como entrada
 
 global count
 count = 0
 
-def contador_pulso(channel):
-   global count
-   if start_counter == 1:
-      count = count+1
-      print count
-      flow = count / (60 * 7.5)
-      print(flow)
+def contador_pulso():
+    global count
+    if GPIO.input(22) == 1:
+        count = count + 1
+        print count
+        flow = (count * 60 * 2.25 / 1000)
+        print "The flow is: %.3f L/m" % (flow)
 
 def guardar_dato(nivel):
     db = MySQLdb.connect(host="34.223.215.43", user="root", passwd="385402292Mica_02", db="control_raspi")
@@ -22,15 +23,12 @@ def guardar_dato(nivel):
     cur.execute("UPDATE control SET nivel_flujo='"+str(nivel)+"'")
     db.commit()
 
-GPIO.add_event_detect(22, GPIO.FALLING, callback=contador_pulso) 
+#GPIO.add_event_detect(22, GPIO.FALLING, callback=contador_pulso) 
 
 try:
 	while True:
-		start_counter = 1
         time.sleep(1)
-        start_counter = 0
-        flow = (count * 60 * 2.25 / 1000)
-        print "The flow is: %.3f Liter/min" % (flow)
+        contador_pulso()
         count = 0
         time.sleep(5)
 except KeyboardInterrupt:
