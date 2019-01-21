38c7833243d095dbed148df643736404b3fb597c
caloloc2 <calolomino@gmail.com>
2019-01-21 17:24:43 -0500

    actualiza

--- nivel.py
+++ nivel.py
@@ -9,6 +9,12 @@ GPIO.setup(22, GPIO.IN) ## GPIO 22 como entrada
 global count
 count = 0
 
+def guardar_dato(nivel):
+    db = MySQLdb.connect(host="34.223.215.43", user="root", passwd="385402292Mica_02", db="control_raspi")
+    cur = db.cursor()
+    cur.execute("UPDATE control SET nivel_flujo='"+str(nivel)+"'")
+    db.commit()
+
 def contador_pulso():
     global count
     for x in range(0, 60):
@@ -16,13 +22,8 @@ def contador_pulso():
             count = count + 1            
             time.sleep(0.01)
     flow = (count * 60 * 2.25 / 1000)
-    print "The flow is: %.3f L/m" % (flow)    
-
-def guardar_dato(nivel):
-    db = MySQLdb.connect(host="34.223.215.43", user="root", passwd="385402292Mica_02", db="control_raspi")
-    cur = db.cursor()
-    cur.execute("UPDATE control SET nivel_flujo='"+str(nivel)+"'")
-    db.commit()
+    print "The flow is: %.3f L/m" % (flow)
+    guardar_dato(flow)
 
 #GPIO.add_event_detect(22, GPIO.FALLING, callback=contador_pulso) 
 
